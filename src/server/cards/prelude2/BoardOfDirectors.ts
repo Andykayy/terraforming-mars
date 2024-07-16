8a021b27ee5e928669841befd6251a000b6f144e
Robert Konigsberg <konigsberg@gmail.com>
2024-07-15 23:37:54 -0400

    Add warnings and logging when someone cannot act on Board of Directors

--- src/server/cards/prelude2/BoardOfDirectors.ts
+++ src/server/cards/prelude2/BoardOfDirectors.ts
@@ -9,6 +9,7 @@ import {SelectCard} from '../../inputs/SelectCard';
 import {message} from '../../logs/MessageBuilder';
 import {PreludesExpansion} from '../../preludes/PreludesExpansion';
 import {SelectPaymentDeferred} from '../../deferredActions/SelectPaymentDeferred';
+import {LogHelper} from '../../LogHelper';
 import {IPreludeCard} from '../prelude/IPreludeCard';
 
 export class BoardOfDirectors extends PreludeCard implements IActionCard {
@@ -50,6 +51,8 @@ export class BoardOfDirectors extends PreludeCard implements IActionCard {
   public action(player: IPlayer) {
     const game = player.game;
     const prelude = game.preludeDeck.drawOrThrow(player.game);
+
+    LogHelper.logDrawnCards(player, [prelude], true);
 
     if (player.canAfford(12)) {
       if (prelude.canPlay?.(player, {cost: 12}) === false) {
