7e6c7fb2c6a0519970723dfd3848456c2af7b4c8
Andrew Kliem <andrewkliem@gmail.com>
2024-09-29 11:54:08 +1000

    Removed tag from advanced alloys

--- src/server/cards/chemical/rebalance/AdvancedAlloysRB.ts
+++ src/server/cards/chemical/rebalance/AdvancedAlloysRB.ts
@@ -1,5 +1,4 @@
 import {IProjectCard} from '../../IProjectCard';
-import {Tag} from '../../../../common/cards/Tag';
 import {Card} from '../../Card';
 import {CardType} from '../../../../common/cards/CardType';
 import {CardName} from '../../../../common/cards/CardName';
@@ -10,8 +9,7 @@ export class AdvancedAlloysRebalance extends Card implements IProjectCard {
   constructor() {
     super({
       type: CardType.ACTIVE,
-      name: CardName.ADVANCED_ALLOYS_RB,
-      tags: [Tag.SCIENCE],
+      name: CardName.ADVANCED_ALLOYS_RB,      
       cost: 9,
 
       behavior: {
