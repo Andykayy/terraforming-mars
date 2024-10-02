dd01cbcfc747181900a35baab0f67a1a07fb144c
Andrew Kliem <andrewkliem@gmail.com>
2024-10-02 23:06:06 +1000

    Updated convertheat method to apply player heat discount to number required

--- src/server/cards/base/standardActions/ConvertHeat.ts
+++ src/server/cards/base/standardActions/ConvertHeat.ts
@@ -24,6 +24,7 @@ export class ConvertHeat extends StandardActionCard {
   }
 
   public canAct(player: IPlayer): boolean {
+    player.heatNeededForTemperature = player.heatNeededForTemperature - player.heatDiscount;
     if (player.game.getTemperature() === MAX_TEMPERATURE) {
       this.warnings.add('maxtemp');
     }
@@ -40,8 +41,7 @@ export class ConvertHeat extends StandardActionCard {
     });
   }
 
-  public action(player: IPlayer) {
-    player.heatNeededForTemperature = player.heatNeededForTemperature - player.heatDiscount;
+  public action(player: IPlayer) {    
     return new SelectOption(
       message('Spend ${0} heat to raise temperature', (b) => b.number(player.heatNeededForTemperature)),
       'Confirm'
