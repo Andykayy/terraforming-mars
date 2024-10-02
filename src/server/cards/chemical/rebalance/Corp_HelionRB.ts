146a09503cd7ac28fb1416a0764e2c5166346f86
Andrew Kliem <andrewkliem@gmail.com>
2024-10-02 23:59:30 +1000

    helion action box

--- src/server/cards/chemical/rebalance/Corp_HelionRB.ts
+++ src/server/cards/chemical/rebalance/Corp_HelionRB.ts
@@ -23,11 +23,11 @@ export class HelionRebalance extends CorporationCard {
         renderData: CardRenderer.builder((b) => {
           b.br;
           b.production((pb) => pb.heat(4).nbsp).megacredits(40);
-          b.corpBox('effect', (ce) => {
+          b.corpBox('action', (ce) => {
             ce.vSpace(Size.LARGE);
             ce.effect('You may use heat as M€. You may not use M€ as heat.', (eb) => {
               eb.startEffect.text('x').heat(1).equals().megacredits(1, {text: 'x'});
-              ce.effect('Using the RAISE TEMPERATURE action costs 1 less heat.', (eb) => eb.temperature(1).asterix().startEffect.minus().heat(1, {text: '1'})).br;              
+              ce.effect('Using the RAISE TEMPERATURE action costs 1 less heat.', (eb) => eb.temperature(1).asterix().startEffect.minus().heat(1.)).br;              
             });
           });
         }),
