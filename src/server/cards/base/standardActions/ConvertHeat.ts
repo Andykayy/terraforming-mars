438bddbf91985297b78b8341192c6fa1e0afed73
Andrew Kliem <andrewkliem@gmail.com>
2024-10-02 23:13:43 +1000

    heatidscount in helion (BASE HELION)

--- src/server/cards/base/standardActions/ConvertHeat.ts
+++ src/server/cards/base/standardActions/ConvertHeat.ts
@@ -24,7 +24,7 @@ export class ConvertHeat extends StandardActionCard {
   }
 
   public canAct(player: IPlayer): boolean {
-    player.heatNeededForTemperature = player.heatNeededForTemperature - player.heatDiscount;
+    
     if (player.game.getTemperature() === MAX_TEMPERATURE) {
       this.warnings.add('maxtemp');
     }
