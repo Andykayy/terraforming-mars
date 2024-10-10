9eed741cd900913152cac79da76928b477bb3300
Andrew Kliem <andrewkliem@gmail.com>
2024-10-10 22:52:09 +1100

    Robinson wild tag

--- src/server/cards/chemical/rebalance/Corp_RobinsonIndustriesRB.ts
+++ src/server/cards/chemical/rebalance/Corp_RobinsonIndustriesRB.ts
@@ -8,11 +8,13 @@ import {CardName} from '../../../../common/cards/CardName';
 import {CardRenderer} from '../../render/CardRenderer';
 import {SelectPaymentDeferred} from '../../../deferredActions/SelectPaymentDeferred';
 import {TITLES} from '../../../inputs/titles';
+import {Tag} from '../../../../common/cards/Tag';
 
 export class RobinsonIndustriesRebalance extends CorporationCard implements IActionCard {
   constructor() {
     super({
       name: CardName.ROBINSON_INDUSTRIES_RB,
+      tags: [Tag.WILD],
       startingMegaCredits: 40,
 
       metadata: {
