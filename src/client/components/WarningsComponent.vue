0eef6a898c18fd76b8280770edb35097e8b9b525
Andrew Kliem <andrewkliem@gmail.com>
2024-10-30 20:52:35 +1100

    Fixed warnings

--- src/client/components/WarningsComponent.vue
+++ src/client/components/WarningsComponent.vue
@@ -24,6 +24,7 @@ const descriptions: Record<Warning, string> = {
   'preludeFizzle': 'This prelude is not playable, so you will discard it and gain 15 M€.',
   'underworldMustExcavateEnergy': 'You will be limited to excavating a space that gives 1 energy production.',
   'deckTooSmall': 'There are not enough cards to complete this action. You will draw fewer cards than expected.',
+  'marsIsTerraformed': 'Note: all global parameters are at their goals.',
 };
 
 export default Vue.extend({
